4b8a259a506d5a20bd94a7842f32007501665ad9
pcmayrp <124546148+pcmayrp@users.noreply.github.com>
2023-04-24 11:42:50 +0700

    Add files via upload

---
+++ tourpoint2.js
@@ -0,0 +1,155 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "สวนสมเด็จพระศรีนครินทร์ฉะเชิงเทรา",
+          "ประเภท": "Natural",
+          "ระดับ": 9
+        },
+        "geometry": {
+          "coordinates": [
+            101.06689202273884,
+            13.68757279681759
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "วัดโสธรวรารามวรวิหาร",
+          "ประเภท": "Culture",
+          "ระดับ": 10
+        },
+        "geometry": {
+          "coordinates": [
+            101.06741266613585,
+            13.673772520250182
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "ตลาดบ้านใหม่ 100 ปี",
+          "ประเภท": "Special",
+          "ระดับ": 8
+        },
+        "geometry": {
+          "coordinates": [
+            101.09009013641918,
+            13.69724139417525
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "คุ้มวิมานดิน",
+          "ประเภท": "Natural",
+          "ระดับ": 8
+        },
+        "geometry": {
+          "coordinates": [
+            101.18719718462239,
+            13.7556403848116
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "วัดโพรงอากาศ",
+          "ประเภท": "Cultural",
+          "ระดับ": 9
+        },
+        "geometry": {
+          "coordinates": [
+            101.05713581055312,
+            13.796726336868062
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "วัดชมโพธาราม",
+          "ประเภท": "Cultural",
+          "ระดับ": 8
+        },
+        "geometry": {
+          "coordinates": [
+            101.04851958874713,
+            13.675433464973764
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "มินิมูร่าห์ ฟาร์ม",
+          "ประเภท": "Natural",
+          "ระดับ": 10
+        },
+        "geometry": {
+          "coordinates": [
+            101.13159978690675,
+            13.603113719037424
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "โรบินสันไลฟ์สไตล์เซ็นเตอร์",
+          "ประเภท": "Special",
+          "ระดับ": 10
+        },
+        "geometry": {
+          "coordinates": [
+            101.0485116338636,
+            13.668440178929217
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "วัดหงษ์ทอง",
+          "ประเภท": "Cultural",
+          "ระดับ": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.87226455088131,
+            13.473699341231509
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "วัดปากน้ำโจ้โล้",
+          "ประเภท": "Cultural",
+          "ระดับ": 9
+        },
+        "geometry": {
+          "coordinates": [
+            101.2091883252312,
+            13.741989875730411
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
